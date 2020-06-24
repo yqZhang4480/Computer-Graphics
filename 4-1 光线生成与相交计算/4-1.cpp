9bb2ae9b744bf50a1dddb89cfa190747e6096487
yqZhang4480 <1073601148@qq.com>
2020-06-24 09:27:08 +0800

    完成 4-1

--- 4-1 光线生成与相交计算/4-1.cpp
+++ 4-1 光线生成与相交计算/4-1.cpp
@@ -10,8 +10,6 @@
 
 #include "../vector3.h"
 
-#include<stdio.h>
-
 /*
  * 4    光线追踪
  * 4-1  光线生成与相交计算
@@ -31,9 +29,11 @@
 //几何对象的父类
 class Surface {
 public:
+    //材质漫反射系数，向量的三个值分别对应RGB通道
+    Vector3 diffuse;
+
     Surface() {}
     ~Surface() {}
-
 
     virtual bool hit(const Vector3& ve,
         const Vector3& vd,
@@ -44,17 +44,17 @@ public:
 
 // 考虑实现球和三角形两个子类；其他几何对象一般可看作若干三角形的拼接。
 
-//漫反射材质的三角形。
+//三角形类
 class Triangle : public Surface {
 public:
     //三角形的三个顶点
     Vector3 p1, p2, p3;
-    //材质漫反射系数，向量的三个值分别对应RGB通道
-    Vector3 diffuse;
 
     Triangle() {}
     Triangle(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 diffuse) 
-    : p1(p1), p2(p2), p3(p3), diffuse(diffuse) {}
+    : p1(p1), p2(p2), p3(p3) {
+        this->diffuse = diffuse;
+    }
 
     /*
      判断光线与三角形是否相交。
@@ -106,25 +106,105 @@ public:
 };
 
 /*
- init_fullscreen  初始化设备屏幕
- 
- 通过一系列操作创建全屏绘图窗口。
+ initFullscreen  初始化全屏绘图窗口
+
+ ***  这些代码与图形学无关  ***
  */
-void init_fullscreen() {
+void initFullscreen() {
     int rW = GetSystemMetrics(SM_CXSCREEN); // 屏幕宽度 像素
     int rH = GetSystemMetrics(SM_CYSCREEN); // 屏幕高度 像素
     initgraph(rW, rH);
-    HWND hwnd = GetHWnd();//获取窗口句柄 以easyx为例
-    LONG l_WinStyle = GetWindowLong(hwnd, GWL_STYLE); // 获取窗口信息
-    // 设置窗口信息 最大化 取消标题栏及边框
+    HWND hwnd = GetHWnd();// EasyX库函数，获取窗口句柄
+    LONG l_WinStyle = GetWindowLong(hwnd, GWL_STYLE);
     SetWindowLong(hwnd, GWL_STYLE,
         (l_WinStyle | WS_POPUP | WS_MAXIMIZE) & ~WS_CAPTION & ~WS_THICKFRAME & ~WS_BORDER);
-    //直接修改窗口样式
     SetWindowPos(hwnd, HWND_TOP, 0, 0, rW, rH, 0);
 }
 
-int main() {
-    init_fullscreen();
 
+int main() {
+    initFullscreen();
+    BeginBatchDraw();
+    Vector3 o(0, 0, 0),
+            a(100, 0, 0),
+            b(0, 100, 0),
+            c(0, 0, 100),
+            color(0.25, 0.5, 0.25);
+    Surface* s[4] = {
+        new Triangle(o, a, b, Vector3(0.75, 0.75, 0.5)),
+        new Triangle(o, b, c, Vector3(0.75, 0.75, 0.5)),
+        new Triangle(o, c, a, Vector3(0.75, 0.75, 0.5)),
+        new Triangle(a, b, c, Vector3(0.75, 0.75, 0.5))
+    };
+    Vector3 e(175, -35, 35);
+    Vector3 e_to_ScreenCenter(-165, 0, 0);  // 从摄像机指向屏幕中心的向量，与屏幕垂直。
+    DWORD* pImg = GetImageBuffer(0); // EasyX库函数，获取窗口显存指针
+    int rW = GetSystemMetrics(SM_CXSCREEN); // 屏幕宽度 像素
+    int rH = GetSystemMetrics(SM_CYSCREEN); // 屏幕高度 像素
+
+    // 计算屏幕坐标系的基
+    Vector3 z(0, 0, 1);
+    Vector3 w_axis, h_axis;
+    Vector3 n = e_to_ScreenCenter;
+    n.normalize();
+    if (n == z || n == -z) {
+        w_axis = Vector3(1, 0, 0);
+    }
+    else {
+        w_axis = v3Cross(n, z);
+    }
+    h_axis = v3Cross(n, w_axis);
+
+    for (int h = 0; h < rH; h++) {
+        for (int w = 0; w < rW; w++) {
+            int index = w + h * rW;
+
+            // 第一步，生成光线。
+            // 关键在于算出光线的方向d。
+            // d == 像素位置 - e
+            // |像素位置 == 屏幕中心 + 偏移
+            // ||屏幕中心 == e + e_to_ScreenCenter
+            // ||偏移 == 屏幕坐标 * 屏幕坐标系的基
+            // 综上，d == e_to_ScreenCenter + 偏移
+            Vector3 bia = w_axis * (w - rW/2) 
+                + h_axis * (h - rH/2);
+            Vector3 d = -(e_to_ScreenCenter + bia);
+            if (bia.length() == 0.0) {
+                int i = 0;
+            }
+
+            // 第二步，判断光线击中的几何对象。
+            float t = 10000000000;
+            float temp;
+            Surface* hitObj = nullptr;
+            for (Surface* o : s) {
+                if (o->hit(e, d, 0, t, temp)) {
+                    if (t > temp) {
+                        t = temp;
+                        hitObj = o;
+                    }
+                }
+            }
+
+
+            // 第三步，在该像素显示几何对象的颜色。
+            if (hitObj != nullptr) {
+                pImg[index] = RGB(
+                    (int)(hitObj->diffuse.z * 255),
+                    (int)(hitObj->diffuse.y * 255),
+                    (int)(hitObj->diffuse.x * 255)
+                );
+                
+            }
+            else {
+                pImg[index] = RGB(64,64,64);
+            }
+        }
+    }
+
+    FlushBatchDraw();
     _getch();
+    delete s[0], s[1], s[2], s[3];
+    s[0] = s[1] = s[2] = s[3] = nullptr;
+    return 0;
 }
